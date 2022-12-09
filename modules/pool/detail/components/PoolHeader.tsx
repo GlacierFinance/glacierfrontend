cea160bd9fdbd17b60cb85da9aaa789500598265
jumpman23 <102991359+jumpman23xx@users.noreply.github.com>
2022-12-09 15:05:02 -0500

    header

--- modules/pool/detail/components/PoolHeader.tsx
+++ modules/pool/detail/components/PoolHeader.tsx
@@ -35,15 +35,32 @@ function PoolHeader() {
     }.`;
 
     return (
-        <VStack width="full" alignItems="flex-start" mb="8">
-            <Text textStyle="h3" as="h3" fontWeight="bold" mr="4" display={{ base: 'block', lg: 'none' }}>
+        <VStack width="full" alignItems="flex-start" justifyContent="center" mb="12" gap="9px">
+            <Text   fontSize="24px"
+                    fontWeight="bold"
+                    textTransform="uppercase"
+                    bg="linear-gradient(to bottom, #F4F1F7 0%, #ECE3F3 25.13%, #AC3DB0 50.9%, #7054E0 58.19%, #C0AFF1 67.05%, #FFFFFF 81.11%, #F6ECFD 90.48%), linear-gradient(180deg, #DFCAD7 16.67%, #A16AB8 49.48%, #BACDFF 75.38%, #5D9988 100%)"
+                    color="transparent"
+                    backgroundClip="text"
+                    border="1.64px solid linear-gradient(180deg, #DFCAD7 16.67%, #A16AB8 49.48%, #BACDFF 75.38%, #5D9988 100%)"
+                    mr="4" display={{ base: 'block', lg: 'none' }}>
                 {pool.name}
             </Text>
-            <Wrap>
-                <WrapItem display={{ base: 'none', lg: 'flex' }}>
-                    <Text textStyle="h3" as="h3" fontWeight="bold" mr="4">
+            <Wrap alignItems="center" justifyContent="center" >
+                <WrapItem display={{ base: 'none', lg: 'flex' }} justifyContent="center" alignItems="center">
+                <h1 style={{
+                fontSize: '36px',
+                alignItems: 'center', justifyContent: 'center', 
+                fontWeight: 'semibold',
+                textTransform: 'uppercase', 
+                background: 'linear-gradient(to bottom, #F4F1F7 0%, #ECE3F3 25.13%, #AC3DB0 50.9%, #7054E0 58.19%, #C0AFF1 67.05%, #FFFFFF 81.11%, #F6ECFD 90.48%), linear-gradient(180deg, #DFCAD7 16.67%, #A16AB8 49.48%, #BACDFF 75.38%, #5D9988 100%)', 
+                WebkitTextFillColor: 'transparent',
+                WebkitBackgroundClip: 'text', 
+                // border: '1.64px solid',
+                // borderImageSource: 'linear-gradient(180deg, #DFCAD7 16.67%, #A16AB8 49.48%, #BACDFF 75.38%, #5D9988 100%)', 
+                }}>
                         {pool.name}
-                    </Text>
+                    </h1>
                 </WrapItem>
                 {pool.displayTokens.map((token, index) => (
                     <WrapItem key={index}>
@@ -51,18 +68,19 @@ function PoolHeader() {
                     </WrapItem>
                 ))}
             </Wrap>
-            <Popover trigger="hover" placement="right">
+            <Popover trigger="hover" placement="right-start">
                 <PopoverTrigger>
                     <HStack
-                        paddingX="3"
-                        paddingY="2"
-                        bg="whiteAlpha.200"
+                        padding="5px"
+                        bg="glacier.silver.400"
+                        fontFamily="JetBrains"
                         spacing="2"
-                        fontSize="md"
-                        rounded="full"
-                        color="beets.base.50"
+                        fontSize="14px"
+                        textTransform="uppercase"
+                        rounded="none"
+                        color="glacier.silver.200"
                         justifyContent="center"
-                        fontWeight="semibold"
+                        fontWeight="500"
                     >
                         {!hasZeroOwner && (
                             <Flex alignItems="center">
@@ -73,14 +91,14 @@ function PoolHeader() {
                                 )}
                             </Flex>
                         )}
-                        <HStack spacing="1">
+                        <HStack spacing="1" >
                             <Text>{numeral(pool.dynamicData.swapFee).format('0.0[00]%')}</Text>
                             <Text>{swapFeeType} Fee</Text>
                         </HStack>
                     </HStack>
                 </PopoverTrigger>
-                <PopoverContent w="200px" bgColor="beets.base.800" shadow="2xl">
-                    <Box p="2" fontSize="sm" bgColor="whiteAlpha.200">
+                <PopoverContent w="200px" bgColor="glacier.silver.400" shadow="4xl">
+                    <Box p="1" fontSize="12px" fontFamily="JetBrains" bgColor="glacier.silver.400">
                         {tooltipText1} {!hasZeroOwner && tooltipText2}
                     </Box>
                 </PopoverContent>
